58e6bbb0671a5014649ef925f48fc319edaa1555
Michael Baker <michaeltbaker@gmail.com>
2012-03-20 14:23:37 +0000

    Servers a static page at root and the AI from everywhere else

--- src/clojure_tictactoe/core.clj
+++ src/clojure_tictactoe/core.clj
@@ -1,14 +1,26 @@
 (ns clojure-tictactoe.core
-  (:import MinMaxPlayer)
-  (:import Move)
-  (:use clojure-webserver.option-parser)
-  (:use clojure-webserver.http-server))
+  (:import MinMaxPlayer
+           Move
+           java.io.File)
+  (:use [clojure-webserver
+         option-parser
+         http-server
+         request-handler]))
 
-(defn handler [env]
+(defn move-handler [env]
   (let [board (drop 1(map str (env :uri)))]
     (println board)
     {:status 200 :headers {} :body "hello"}))
 
+(defn make-dispatcher [file-handler move-handler]
+  (fn [env]
+    (cond
+      (.startsWith (:uri env) "/public") (file-handler env)
+      (= (:uri env) "/") (file-handler (assoc env :uri "/public/index.html"))
+      :otherwise (move-handler env))))
+
 (defn -main [& args]
-  (let [[directory port] (parse-options args)]
-    (start-server port handler)))
+  (let [[directory port] (parse-options args)
+        file-handler     (request-handler (File. directory))
+        request-dispatch (make-dispatcher file-handler move-handler)]
+    (start-server port request-dispatch)))
