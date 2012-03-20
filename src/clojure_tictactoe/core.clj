5ba23fc76cdab3d0d32d019a90d9d32266321c73
Michael Baker <michaeltbaker@gmail.com>
2012-03-20 15:08:39 +0000

    Server responds to moves

--- src/clojure_tictactoe/core.clj
+++ src/clojure_tictactoe/core.clj
@@ -7,17 +7,56 @@
          http-server
          request-handler]))
 
+(defn str-to-token [token]
+  (if (= token \_)
+    nil
+    token))
+
+(defn char-to-token [character]
+  (if (= character nil)
+    nil
+    (str character)))
+
+(defn token-to-str [token]
+  (if (= nil token)
+    "_"
+    token))
+
+(defn string-to-board [string]
+  (->> (.substring string 1)
+    (map str-to-token)
+    (map char-to-token)
+    (into-array String)))
+
+(defn move-to-index [move]
+  (let [row    (.getRow move)
+        column (.getColumn move)]
+    (+ (* row 3) column)))
+
+(defn new-board [board move]
+  (let [index (move-to-index move)
+        token "O"
+        board (vec board)]
+  (->> (assoc board index token)
+    (map token-to-str)
+    (reduce str ""))))
+
 (defn move-handler [env]
-  (let [board (drop 1(map str (env :uri)))]
-    (println board)
-    {:status 200 :headers {} :body "hello"}))
+  (let [board   (string-to-board (:uri env))
+        ai-move (.nextMove (MinMaxPlayer. board "O" "X"))]
+    {:status 200
+     :headers {}
+     :body (new-board board ai-move)}))
 
 (defn make-dispatcher [file-handler move-handler]
   (fn [env]
     (cond
-      (.startsWith (:uri env) "/public") (file-handler env)
-      (= (:uri env) "/") (file-handler (assoc env :uri "/public/index.html"))
-      :otherwise (move-handler env))))
+      (.startsWith (:uri env) "/public")
+        (file-handler env)
+      (= (:uri env) "/")
+        (file-handler (assoc env :uri "/public/index.html"))
+      :otherwise
+        (move-handler env))))
 
 (defn -main [& args]
   (let [[directory port] (parse-options args)
--- test/clojure_tictactoe/test/core.clj
+++ test/clojure_tictactoe/test/core.clj
@@ -1,6 +1,18 @@
 (ns clojure-tictactoe.test.core
+  (:import java.util.Arrays)
   (:use [clojure-tictactoe.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn nils [amount]
+  (take amount (repeat nil)))
+
+(testing "string-to-board"
+  (deftest empty-board
+    (is (Arrays/equals
+          (into-array String (nils 9))
+          (string-to-board "/_________"))))
+
+  (deftest one-X
+    (is (Arrays/equals
+          (into-array String (cons "X" (nils 8)))
+          (string-to-board "/X________")))))
