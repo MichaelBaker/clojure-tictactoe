f1bc0e7f55749a20513f7906fc856e2373fad017
Michael Baker <michaeltbaker@gmail.com>
2012-03-22 14:43:09 +0000

    Fixes a bug in the AI

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description  "FIXME: write description"
   :dependencies [[org.clojure/clojure       "1.3.0"]
                  [michael/clojure-webserver "1.0.0"]
-                 [michael/min-max-player    "1.0.0"]
+                 [michael/min-max-player    "1.0.2"]
                  [michael/move              "1.0.0"]
                  [org.clojure/data.json     "0.1.2"]]
   :main         clojure-tictactoe.core)
